7bbc46f4c49383f41d0349a6e8a163e95d8e4a28
Tom Armitage <tom@infovore.org>
2015-02-10 18:16:23 +0000

    Tweaks to mapping an dloader.

--- es_loader.rb
+++ es_loader.rb
@@ -16,6 +16,10 @@ Dir["json/*/*.json"].each do |json_file|
     data.lastStarport.modules = data.lastStarport.modules.map {|k,v| v}
   end
 
+  if data.lastStarport.ships
+    data.lastStarport.ships = data.lastStarport.ships.map {|k,v| v}
+  end
+
   data.timestamp = Time.at(data.timestamp).to_datetime.iso8601
 
   client.index(index: 'ed_logs', type: 'ed_log', body: data.to_json)
